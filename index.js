720c6eba9c512ae94de5a04952e25531b8394600
Jotanjo <maza011000@gmail.com>
2022-11-19 20:36:19 -0300

    la real function

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 function laCajaDePandora(numero){
     if(typeof numero !== "number") throw new Error("you must not give us something different than a number")
     if(numero % 2 === 0){
-        //me muero con las gatas de juanjo
+        return (numero).toString(2)
+        }
+    if(numero % 2 === 1){
+        return (numero).toString(16)
     }
-};
+}
+
+function Joaquin() {
+    return{
+        name: "Joaquin Alejo Maza",
+        age: 22,
+        nationality: "Argentinian"
+    }
+}
