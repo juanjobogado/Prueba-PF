56ed0442573e7e1231ea6d22497eea8a74505695
juanjobogado <104685460+juanjobogado@users.noreply.github.com>
2022-11-19 19:00:02 -0300

    Delete index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-function laCajaDePandora(numero){
-    if(typeof numero !== "number") throw new Error("you must not give us something different than a number")
-    if(numero % 2 === 0){
-        
-    }
-}
