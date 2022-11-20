a975a87238005b36f01329adb17c03df06a9c6f5
userJAMT <jmendez4tob@gmail.com>
2022-11-20 19:58:39 -0300

    index de jose

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 function laCajaDePandora(numero){
     if(typeof numero !== "number") throw new Error("you must not give us something different than a number")
-    if(numero % 2 === 0){
-        //me muero con las gatas de juanjo
-    }
+    if(numero % 2 === 0) return numero.toString(2)
+    else return numero.toString(16)
 };
