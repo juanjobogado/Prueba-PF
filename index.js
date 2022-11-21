7e8973f1d3e90800f2d0852607637e7e6ed273bc
NicoxRC <nicorojas005@gmail.com>
2022-11-21 08:41:59 -0500

    commit nicolas

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-function laCajaDePandora(numero){
-    if(typeof numero !== "number") throw new Error("you must not give us something different than a number")
-    if(numero % 2 === 0){
-        //me muero con las gatas de juanjo
+function laCajaDePandora(numero) {
+    if (typeof numero !== "number") throw new Error("you must not give us something different than a number");
+    if (numero % 2 === 0) {
+        return numero.toString(2);
+    } else {
+        return numero.toString(16);
     }
 };
+
+
