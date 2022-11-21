ec59a1da3a277b267fa64c8ef0ec00916901a619
Jotanjo <maza011000@gmail.com>
2022-11-21 16:47:31 -0300

    la real function

--- index.js
+++ index.js
@@ -5,33 +5,8 @@ function laCajaDePandora(numero){
     else return numero.toString(16)
 };
 
-function jose(){
-    let object = {nombre: "Jose", edad: 25, nacionalidad: "Venezolano"}
-    return object
-}
-
-function Martina() {
-    return{
-        name: "Martina Girollet",
-        age: 24,
-        nationality: "Argentinian"
-    }
-};
-
-function juanjo(){
-    let data = {
-        name: "juan",
-        age: 23,
-        nationality: argentinian
-
-    }
-    return data;
-};
-
-const nicolas = () => {
-    return {
-        nombre: "Nicolas Rojas",
-        edad: 22,
-        pais: "Colombia"
-    }
+function Joaquin(){
+    return{name: "Joaquin Maza",
+    age: 22,
+    nationality: "Argentinian"}
 }
